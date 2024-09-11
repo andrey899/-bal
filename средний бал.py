31cc009988b0f2b4a8a617df80d5aa5d4eb500ae
andrey899 <andrey291281@mail.ru>
2024-09-11 21:33:39 +0500

    Add files via upload

--- средний бал.py
+++ средний бал.py
@@ -2,7 +2,7 @@
 
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 4, 5]] # список оценок
 print(type(grades))
-students = {'Johnny', 'Bilbo', 'Khendrik', 'Aaron'} # множество неупорядочное последовательность имен
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'} # множество неупорядочное последовательность имен
 
 print(type(students))
 m = sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]), sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4])
